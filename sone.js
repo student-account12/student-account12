3aa4c2aefce75f43b07db517f823bb56fcd9174e
mesend48 <mesend48@gmail.com>
2025-01-02 11:43:03 -0600

    added js file

---
+++ sone.js
@@ -0,0 +1,9 @@
+// This is a simple JavaScript script
+
+// Function to greet the user
+function greetUser(name) {
+    console.log(`Hello, ${name}! Welcome to your JavaScript file.`);
+}
+
+// Call the function with a sample name
+greetUser('Student');
